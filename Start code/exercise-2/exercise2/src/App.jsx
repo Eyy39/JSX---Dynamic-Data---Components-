095ec5093cc172d895bbfd2a43728b03db10e342
Eyy39 <phonsovaney8@gmail.com>
2026-02-12 16:13:33 +0700

    week4

---
+++ Start code/exercise-2/exercise2/src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './Header'
+import Mains from './Mains'
+import Footer from './Footer'
+function App() {
+  return (
+    <>
+      <p>Hello</p>
+      <Header/>
+      <Mains/>
+      <Footer/>
+    </>
+  );
+}
+export default App;
+
